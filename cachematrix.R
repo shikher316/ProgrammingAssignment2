f2a68e3395253ad8ffb440fc1ed75fea18f53971
shikher316 <shikher.verma@gmail.com>
2017-01-15 20:08:23 -0600

    Final_Submission
    Reverse Matrix submission

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,13 @@
 
 ## Write a short comment describing this function
 
+# Function to create a special vector which consist of list of sub functions for the purpose of calculating the inverse of the matrix 
+# The function consists of the following inline functions - 
+# Get the value of the special vector 
+# Set the value of the Vector 
+# Get the value of the inverse of matrix
+# Set the value inverse of matrix 
+
 makeCacheMatrix <- function(x = matrix()) {
   inverse_temp <- NULL
   set <- function(y) {
@@ -10,8 +17,11 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
   get <- function() x
+  # defining function to set the value of inverse of matrix         
   setinverse <- function(solve) inverse_temp <<- solve
-  getinversE <- function() inv
+  # defining function to get the value of inverse of matrix         
+  getinverse <- function() inv
+  # defining the vector list 
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -21,16 +31,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# Function to call the inverse function of matric. In case the matrix is already present in the cache, the pre-calculated value for the inverse of the matrix will be printed
+# In case there is no inverse present for the matrix, the inverse will be calculated and stored for further computation 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #calling the function to get the cached data set 
   m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  #In case the there is no cached matrix inverse, calculate the dataset and save it for future reference
   data <- x$get()
+  # calling solve function to calculate the inverse 
   m <- solve(data, ...)
+  # storing the matrix calculation in case cache was not available
   x$setmean(m)
   m
 }
